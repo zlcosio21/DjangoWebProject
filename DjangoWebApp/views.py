f1196b394fc958e965ddc46fea2f16f585360a6a
Sebastian <sebas60621@gmail.com>
2023-08-08 00:22:52 -0500

    Estructura de las vistas, templates y URls

---
+++ DjangoWebApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+
+    return render(request, "home.html")
+
+
+def servicios(request):
+
+    return render(request, "servicios.html")
+
+
+def tienda(request):
+
+    return render(request, "tienda.html")
+
+
+def blog(request):
+
+    return render(request, "blog.html")
+
+
+def contacto(request):
+
+    return render(request, "contacto.html")

a14e47282e159c847ff65e26cbb567440173c756
Sebastian <sebas60621@gmail.com>
2023-08-08 14:49:20 -0500

    Agregando rutas del aplicacion DjangoWebApp

--- DjangoWebApp/urls.py
+++ DjangoWebApp/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from DjangoWebApp.views import home, servicios, tienda, contacto
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('DjangoWebApp.urls'))
+    path('home/', home),
+    path('servicios/', servicios),
+    path('tienda/', tienda),
+    path('contacto/', contacto),
 ]

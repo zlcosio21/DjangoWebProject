6a0c26a72bc3d1a632b0d6ad0826587807aec6b4
Sebastian <sebas60621@gmail.com>
2023-08-09 20:43:04 -0500

    Mostrar imagenes en los registros

--- DjangoWebApp/urls.py
+++ DjangoWebApp/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
 from DjangoWebApp import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('home/', views.home, name='home'),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('contacto/', views.contacto, name='contacto'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

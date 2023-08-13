fc4187e7d29768b9a2c83d32bb5e65ecf329795e
Sebastian <sebas60621@gmail.com>
2023-08-13 00:53:03 -0500

    Configurando Mysql a Django

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'DjangoWebProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangowebproject',
+        'USER': 'root',
+        'PASSWORD': 'minato21',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

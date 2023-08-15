792ad1c1e0d427246b3cc7fe4ae9f146fdbfdd79
Sebastian <sebas60621@gmail.com>
2023-08-14 19:59:16 -0500

    Mostrando posts en el sitio web

--- blog/models.py
+++ blog/models.py
@@ -14,13 +14,14 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
     
+
 class Post(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=50)
     imagen = models.ImageField(upload_to='blog', null=True, blank=True)
 
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    Categorias = models.ManyToManyField(Categoria)
+    categorias = models.ManyToManyField(Categoria)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)

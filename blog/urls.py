5d98554aa9c5ad58a222f521ed492da35d80a546
Sebastian <sebas60621@gmail.com>
2023-08-15 14:18:21 -0500

    Ver posts por categoria

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('categoria/<int:categoria_id>/', views.categoria, name='categoria'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,13 @@ from blog.models import Post, Categoria
 def blog(request):
 
     posts = Post.objects.all()
-    categorias = Categoria.objects.all()
 
-    return render(request, "blog/blog.html", {"posts":posts, "categorias":categorias})
+    return render(request, "blog/blog.html", {"posts":posts})
+
+
+def categoria(request, categoria_id):
+    
+    categoria = Categoria.objects.get(id = categoria_id)
+    posts = Post.objects.filter(categorias = categoria)
+
+    return render(request, "blog/categoria.html", {"categoria":categoria, "posts":posts})

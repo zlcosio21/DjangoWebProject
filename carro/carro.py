b3529a7e937c136697c72836411bcf8eddb130c5
Sebastian <sebas60621@gmail.com>
2023-08-19 17:30:22 -0500

    Metodo agregar productos

--- carro/carro.py
+++ carro/carro.py
@@ -9,3 +9,13 @@ class Carro:
             carro = self.session["carro"] = {}
         else:
             self.carro = carro
+
+    def agregar(self, producto):
+        if(str(producto.id) not in self.carro.keys()):
+            self.carro[producto.id] = {
+                "producto.id":producto.id,
+                "nombre":producto.nombre,
+                "precio":str(producto.precio),
+                "cantidad":1,
+                "imagen":producto.imagen.url,
+            }

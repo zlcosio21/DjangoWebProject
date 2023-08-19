23b51cb214595deadc05d8e240377e054009b092
Sebastian <sebas60621@gmail.com>
2023-08-18 21:43:29 -0500

    Ocultando SECRET_KEY de Django

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -18,17 +18,17 @@ from dotenv import load_dotenv
 load_dotenv()
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
+PASSWORD_DJANGO = os.getenv("PASSWORD_DJANGO")
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-samf=w*_=w^beq%xvfj^g%h6v4t6i@of2%&_f40cu5$#h-vhmz'
+SECRET_KEY = PASSWORD_DJANGO
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

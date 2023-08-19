c5a7c3d8600c98e8b46b3b7a6b879775b3137188
Sebastian <sebas60621@gmail.com>
2023-08-18 22:14:02 -0500

    Creacion de modelos App 'tienda'

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class CategoriaProd(models.Model):
+    nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        verbose_name = 'categoria_producto'
+        verbose_name_plural = 'categorias_producto'
+
+    def __str__(self):
+        return self.nombre
+    
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.FloatField()
+    imagen = models.ImageField(upload_to='tienda', null=True, blank=True)
+    categorias = models.ForeignKey(CategoriaProd, on_delete=models.CASCADE)
+    disponibilidad = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        verbose_name = 'producto'
+        verbose_name_plural = 'productos'
+
+    def __str__(self):
+        return self.nombre

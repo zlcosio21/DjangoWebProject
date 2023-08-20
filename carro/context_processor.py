9f31f9ec4f8b0ad3943aaaf7c40c84584c5b9445
Sebastian <sebas60621@gmail.com>
2023-08-20 12:06:35 -0500

    Corrigiendo variable global

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,7 +1,8 @@
 def importe_total_carro(request):
     total = 0
-    if request.user.is_aunthenticated:
-        for key, value in request.session["carro"].items():
-            total = total + (float(value["precio"]) * value["cantidad"])
+    if request.user.is_authenticated:
+        if "carro" in request.session:
+            for key, value in request.session["carro"].items():
+                total = total + (float(value["precio"]) * value["cantidad"])
 
-    return {"importe_total_carro":total}
+    return {"importe_total_carro": total}

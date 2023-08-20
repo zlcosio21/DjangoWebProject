e3a8224ad17a23f3736020882132ef6806275f16
Sebastian <sebas60621@gmail.com>
2023-08-20 11:55:14 -0500

    Variable global para el total de los productos

---
+++ carro/context_processor.py
@@ -0,0 +1,7 @@
+def importe_total_carro(request):
+    total = 0
+    if request.user.is_aunthenticated:
+        for key, value in request.session["carro"].items():
+            total = total + (float(value["precio"]) * value["cantidad"])
+
+    return {"importe_total_carro":total}

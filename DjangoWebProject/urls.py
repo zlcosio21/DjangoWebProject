6ac4f69d0c4f986d07f6ec01c4f2f405eab6a1c8
Sebastian <sebas60621@gmail.com>
2023-08-20 19:47:05 -0500

    Widget para el carro de la tienda

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('contacto/', include('contacto.urls')),
     path('tienda/', include('tienda.urls')),
+    path('carro/', include('carro.urls')),
     
 ]

f7715ac0dc3608b2cfd3665e896c594ced1ca90b
Sebastian <sebas60621@gmail.com>
2023-08-21 00:06:40 -0500

    Corrigiendo suma del total de los productos

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,8 +1,9 @@
 def importe_total_carro(request):
     total = 0
+    
     if request.user.is_authenticated:
         if "carro" in request.session:
             for key, value in request.session["carro"].items():
-                total = total + (float(value["precio"]) * value["cantidad"])
-
+                total = total + float(value["precio"])
+                
     return {"importe_total_carro": total}

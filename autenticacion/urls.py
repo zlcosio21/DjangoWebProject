f3d7df0c7efea87e6a245e3d6b869c9a9174e60b
Sebastian <sebas60621@gmail.com>
2023-08-22 22:25:02 -0500

    Crenado formulario de registro usando UserCreationForm

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from autenticacion import views
+from .views import VistaRegistro
 
 urlpatterns = [
-    path('', views.autenticacion, name='autenticacion'),
+    path('', VistaRegistro.as_view(), name='autenticacion'),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def autenticacion(request):
+class VistaRegistro(View):
 
-    return render(request, "registro/registro.html")
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "registro/registro.html", {"form":form})
+
+    def post(self, request):
+        pass

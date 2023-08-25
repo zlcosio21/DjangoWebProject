2e462be6540d5dbffc27891cdaafc41c3a19cca2
Sebastian <sebas60621@gmail.com>
2023-08-24 20:55:10 -0500

    Realizando los modelos necesarios de la App 'pedidos'

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from tienda.models import Producto
+from django.db.models import F, Sum, FloatField
 
 # Create your models here.
+
+User = get_user_model()
+
+class Pedido(models.Model):
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        db_table = 'pedidos'
+        verbose_name = 'pedido'
+        verbose_name_plural = 'pedidos'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.id
+
+    @property
+    def total(self):
+        return self.lineapedido_set.aggregate(
+
+            total = Sum(F("precio") *F("cantidad"), outpot_field = FloatField())
+
+        )["total"]
+
+
+class LineaPedido(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    pedido_id = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        db_table = 'lineapedido'
+        verbose_name = 'linea_pedido'
+        verbose_name_plural = 'linea_pedidos'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.cantidad} unidades de {self.producto_id.nombre}"

70b01e595412587dfb80571f3fa86533851309f3
Sebastian <sebas60621@gmail.com>
2023-08-24 21:03:20 -0500

    Incluyendo los modelos de 'pedidos' en el panel de admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Pedido, LineaPedido
 
 # Register your models here.
+class PedidoAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at']
+
+class LineaPedidoAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at']
+
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(LineaPedido, LineaPedidoAdmin)

53c93d9842f47c379f5337987cf745a08237eae7
Sebastian <sebas60621@gmail.com>
2023-08-25 23:55:06 -0500

    Vista para el proceso de pedido

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from pedidos.models import Pedido, LineaPedido
+from carro.carro import Carro
+from django.contrib import messages
 
 # Create your views here.
+@login_required(login_url = "autenticacion/iniciar_seson")
+def procesar_pedido(request):
+    
+    pedido = Pedido.objects.create(user = request.user)
+    carro = Carro(request)
+
+    lineas_pedido = list()
+    for key, value in carro.carro.items():
+        lineas_pedido.append(LineaPedido(
+
+            producto_id = key,
+            cantidad = value["cantidad"],
+            user = request.user,
+            pedido = pedido
+
+        ))
+
+    LineaPedido.objects.bulk_create(lineas_pedido)
+
+    messages.success(request, "El pedido ha sido un exito")
